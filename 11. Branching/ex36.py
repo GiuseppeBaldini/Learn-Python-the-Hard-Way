4f7a2de73dabfb12b1a284290a9af8569897c784
Giuseppe Baldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2018-11-02 21:37:50 +0100

    Minor changes

--- 11. Branching/ex36.py
+++ 11. Branching/ex36.py
@@ -6,7 +6,7 @@ al momento. Lei con chi si schiera: Renzi o Speranza?"""
 
     choice = raw_input("Scriva pure qui: ")
     if choice == "Renzi" or choice == "renzi":
-        print """Ti piace vincere facile, eh? Buone notizie, piaci a Renzi.
+        print """Buone notizie, piaci a Renzi.
 Quante migliaia di voti credi di poter prendere a livello nazionale?"""
 
         voti = int(raw_input("Numero di voti in migliaia: "))
@@ -21,19 +21,19 @@ Quante migliaia di voti credi di poter prendere a livello nazionale?"""
         else:
             not_elected("Mi sembrava di aver chiesto un numero.")
     elif choice == "Speranza" or choice == "speranza":
-        print "Certo che hai proprio la faccia come il culo!"
-        not_elected("E poi, non puoi mica pretendere di essere eletto con Speranza")
+        print "Dai, va bene cosi!"
+        not_elected("Niente da fare, non eletto.")
     else:
-        print "Impari a leggere, avevo chiesto Renzi o Speranza."
-        not_elected("Spiacente, ma di analfabeti in politica ne abbiamo abbastanza.")
+        print "Avevo chiesto Renzi o Speranza."
+        not_elected("Spiacente, ma di analfabeti non possiamo eleggerne.")
 
 def m5s():
     print "Benvenuto al tuo primo meetup M5S. Hai mai fatto parte di partiti politici?"
     choice = raw_input("Si o no? ")
     if choice == "si":
-        not_elected("Vergogna uomo della casta, torna nel tuo partito!")
+        not_elected("Mi dispiace, non é ammesso!")
     elif choice == "no":
-        print "Sembri un tipo apposto. Benvenuto nel Movimento!"
+        print "Va bene. Benvenuto nel Movimento!"
         exit(0)
     else:
         not_elected("Le avevo chiesto si o no, non mi sembrava di aver chiesto molto.")
@@ -43,29 +43,28 @@ def fi():
     print "Ma prima, una domanda: sei mai stato indagato?"
     choice = raw_input("Si o no? ")
     if choice == "si":
-        print """Ottimo! Ti troverai molto bene qui. Fai come fossi a casa tua.
+        print """Capisco.
         Consigli:
-        1) Parla lentamente con Gasparri, a volte non capisce se parli troppo
-        veloce.
-        2) Attento a non calpestare Brunetta"""
+        1) Accetta gli inviti a cena
+        2) Attento a non far incazzare nessuno"""
         exit(0)
     elif choice == "no":
-        not_elected("Ma ci ha preso per gente onesta?!")
+        not_elected("Mmmmm, okay.")
     else:
         not_elected("Le avevo chiesto si o no, non mi sembrava di aver chiesto molto.")
 
 def new():
     print "Lei ha indubbiamente del fegato! Come si chiama questo nuovo partito?"
     choice = raw_input("Scriva qui >> ")
-    print "%s, dice? Guardi, con un nome cosi prende meno voti di Casini." %choice
+    print "%s, dice? Guardi, con un nome cosi la vedo male." %choice
     exit(0)
 
 def not_elected(why):
     print why, "E ora fuori di qui, vada a fare il portaborse!"
 
 def start():
-    print """Benvenuto nel mondo della politica italiana. Si sieda comodo e
-stia attento a non calpestare Brunetta.
+    print """Benvenuto nel mondo della politica italiana. Si sieda comodo
+ma non troppo.
 Come sa, le elezioni si avvicinano, con chi si candida?
 PD, Movimento 5 Stelle (M5S), Forza Italia o nuovo partito?"""
 
@@ -80,6 +79,6 @@ PD, Movimento 5 Stelle (M5S), Forza Italia o nuovo partito?"""
     elif choice == "nuovo partito" or choice == "nuovo":
         new()
     else:
-        not_elected("Iniziamo bene, non sa neanche scegliere.")
+        not_elected("Iniziamo bene.")
 
 start()
